7013dad4ff711e8c9a4a7b71e7852e8efc15f3fc
Soupy <84595880+Soupyx@users.noreply.github.com>
2024-01-23 09:37:52 +0100

    depart du projet

---
+++ Character/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@export var speed : float = 300.0
+@export var jump_velocity : float = -400.0
+@export var double_jump_velocity : float = -350
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped : bool = false 
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		has_double_jumped = false
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif not has_double_jumped:
+			velocity.y = double_jump_velocity
+			has_double_jumped = true
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	# Move and slide with a custom floor direction to make the character lower.
+	move_and_slide()
